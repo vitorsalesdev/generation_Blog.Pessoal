6ff02f6dccb9d553e85c8655a087e0eaa64e357a
Vitor Sales <vitorsalesdev@gmail.com>
2020-11-05 21:20:21 -0300

    Segurança e service ok

--- blogPessoalFront/src/app/cadastro/cadastro.component.ts
+++ blogPessoalFront/src/app/cadastro/cadastro.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../model/User';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User();
+  senha: string;
 
-  ngOnInit(): void {
+  constructor(
+    private authservice: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
   }
 
+  conferirSenha(event: any){
+    this.senha = event.target.value
+  }
+
+  cadastrar(){
+    if(this.senha===this.user.senha){
+      this.authservice.cadastrar(this.user).subscribe((resp: User) => {
+        this.user = resp
+        this.router.navigate(['/login'])
+        alert("Cadastro efetuado com sucesso!")
+      }) 
+    } else {
+      alert("Suas senhas não conferem")
+    }
+  }
 }
